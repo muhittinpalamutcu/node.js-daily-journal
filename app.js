4c243a24142984499cc4dd784e8a3f8d46ef93c2
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2020-07-25 20:33:22 +0300

    Add new get requests

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
-const date=require(__dirname+"/date.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 
 
-const app=express();
+const app = express();
 
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-const posts=[];
+const posts = [];
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,32 +21,46 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 
 
-app.get("/",function(req,res){
-  res.render("home",{
-    todayDate:date.getDate(),
-    homeContent:homeStartingContent,
-    postList:posts
+app.get("/", function(req, res) {
+  res.render("home", {
+    todayDate: date.getDate(),
+    homeContent: homeStartingContent,
+    postList: posts
   });
 });
 
-app.get("/compose",function(req,res){
-  res.render("posts",{
-    todayDate:date.getDate()
+app.get("/compose", function(req, res) {
+  res.render("posts", {
+    todayDate: date.getDate()
   });
 });
 
-app.post("/compose",function(req,res){
-  let title=req.body.postTitle;
-  let postBody=req.body.postContent;
-  const post={
-    title:title,
-    post:postBody
+app.get("/about", function(req, res) {
+  res.render("about", {
+    todayDate: date.getDate(),
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    todayDate: date.getDate(),
+    contactContent: contactContent
+  });
+});
+
+app.post("/compose", function(req, res) {
+  let title = req.body.postTitle;
+  let postBody = req.body.postContent;
+  const post = {
+    title: title,
+    post: postBody
   };
   posts.push(post);
   console.log(post);
   res.redirect("/");
 });
 
-app.listen(3000,function(req,res){
+app.listen(3000, function(req, res) {
   console.log("Server started on port 3000");
 });
